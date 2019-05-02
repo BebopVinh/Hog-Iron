7e7c9c8149aea315966936783155ba4facebe37a
bbennett7 <bryn.bennett@gmail.com>
2019-05-02 16:40:17 -0700

    Added files and routes for Home page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :teachers
   resources :klasses
   resources :students
+
+
+  get '/hogwarts', to: 'application#index'
+  get '/', to: 'application#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
