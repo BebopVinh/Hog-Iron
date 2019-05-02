8ed81f6de30c489767e38ec7fd910e5922f48b33
bbennett7 <bryn.bennett@gmail.com>
2019-05-02 15:48:24 -0700

    House index and show actions and views

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,13 @@
 class House < ApplicationRecord
   has_many :students
   belongs_to :teacher, optional: true
+
+  def slug
+    self.name.downcase.strip.gsub(/[^a-z0-9\-\b]/, "-")
+  end
+
+  def self.find_by_slug(name)
+    House.find{|house| house.slug == house}
+  end
+  
 end
